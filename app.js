fbd3ae09eceddeb53c08c6f54102ad7ab2ab7a85
eselse <ilikewebdevelopment@gmail.com>
2021-06-03 19:52:34 +0400

    add new function for saving data in local storage

--- app.js
+++ app.js
@@ -6,13 +6,25 @@ const movieSelect = document.getElementById("movie");
 // const ticketPrice = +movieSelect.value;
 let ticketPrice = parseInt(movieSelect.value);
 
+// Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex)
+    localStorage.setItem('selectedMoviePrice', moviePrice)
+}
+
+
 // Update total and count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
 
   //   Copy selected seats into arr
   //   Map through array
-  //   return a new array
+  //   return a new array of indexes
+
+  //   Convert nodelist into array
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
 
   const selectedSeatsCount = selectedSeats.length;
   count.innerText = selectedSeatsCount;
@@ -21,6 +33,7 @@ function updateSelectedCount() {
 
 // Movie select event
 movieSelect.addEventListener("change", (e) => {
+  setMovieData(e.target.selectedIndex, e.target.value);
   // ticketPrice = +e.target.value
   ticketPrice = parseInt(e.target.value);
   updateSelectedCount();
